a9808a1765defc80c20cc6d3838ff391678220dc
pmigoku <36520122+pmigoku@users.noreply.github.com>
2025-11-02 09:27:37 -0500

    Add IP monitoring script for LAN and WAN addresses

---
+++ ip-monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get internal IP address
+hostIP="$(hostname -I)"
+lanIP="$(echo "${hostIP}" | awk '{print $1}')"
+
+# Get external IP address
+wanIP="$(curl ifconfig.me 2>/dev/null)"
+
+# Create text output
+textOuts="LAN: ${lanIP} | WAN: ${wanIP}"
+
+# Get TUN device info
+tunDev=$(ip a show tun0 2>&1)
+
+# Test if device exists
+if [[ ${tunDev} != *"not exist"* ]]; then
+	# Get TUN IP address
+	tunIP=$(echo "$tunDev" | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+
+	# Append text output
+	textOuts+=" | VPN: ${tunIP}"	
+fi
+
+# Print final plain text output
+echo "${textOuts}"
